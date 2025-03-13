768c528dab1bc486bce8885f5300a65e36f991d0
imamnurby <imamnurby@users.noreply.github.com>
2025-03-13 22:58:58 +0800

    move functions to implement into 1 file

--- server/pointing_direction_solver_fromPCD.py
+++
@@ -1,7 +0,0 @@
-'TODO: You should implement this'
-def pointing_direction_solver(mask,predictions,depth_image,t_matrx,intrinsic_matrx):
-   
-
-  x1,y1,z1 = 0,0,0
-  V = [0,0,0]
-  return x1,y1,z1,V
